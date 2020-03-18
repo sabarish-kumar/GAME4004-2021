37b68678108f13b0fd6f4f5e8f6c4dd2dc9d98f6
Rob Bogucki <robbogucki@gmail.com>
2020-03-18 01:21:46 +0000

    bugfixes: fixed player collider, added gun timer, corrected proximity checks

--- GameProgramming_2020/Assets/Scripts/TurretController.cs
+++ GameProgramming_2020/Assets/Scripts/TurretController.cs
@@ -10,6 +10,8 @@ public class TurretController : MonoBehaviour
     public Transform domeXform;
     public Transform aimTarget;
     public GameObject bulletPrefab;
+    public float gunInterval = 0.1f;
+    private float gunTimer = 0.0f;
 
     public LayerMask raycastMask;
 
@@ -23,9 +25,23 @@ public class TurretController : MonoBehaviour
 
     private void Update()
     {
+        gunTimer -= Time.deltaTime;
+
         if (alerted)
         {
             interpolator = Mathf.Lerp(interpolator, 1.0f, 0.1f);
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(gunXform.position, gunXform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, raycastMask))
+            {
+                if ((hit.collider.gameObject.tag=="Player")&&(gunTimer<0.0f))
+                {
+                    Instantiate(bulletPrefab, gunXform.position, Quaternion.identity);
+                    gunTimer = gunInterval;
+                }
+
+            }
         }
         else
         {
@@ -37,30 +53,23 @@ public class TurretController : MonoBehaviour
         //    Instantiate(bulletPrefab, gunXform.position, Quaternion.identity);
         //}
 
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(gunXform.position, gunXform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, raycastMask))
-        {
-            if (hit.collider.gameObject.tag=="Player")
-            Instantiate(bulletPrefab, gunXform.position, Quaternion.identity);
-        }
-
         Vector3 aimVec = aimTarget.position - gunXform.position;
         Quaternion gunQuat = Quaternion.LookRotation(aimVec);
         gunXform.rotation = Quaternion.Slerp(Quaternion.identity, gunQuat, interpolator);
         aimVec.y = 0.0f;
         Quaternion domeQuat = Quaternion.LookRotation(aimVec);
         domeXform.rotation = Quaternion.Slerp(Quaternion.identity, domeQuat, interpolator);
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        alerted = true;
+        if (other.tag=="Player")
+            alerted = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        alerted = false;
+        if (other.tag == "Player")
+            alerted = false;
     }
 }
